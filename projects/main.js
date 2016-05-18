a6bfe2cdfb8db3d37b31b9b90e6935d9e492bc36
komali2 <rogersjcaleb@gmail.com>
2016-05-18 10:39:29 -0700

    core: broke out modularity further

--- projects/main.js
+++ projects/main.js
@@ -1,6 +1,10 @@
 import summer from './threeandfive.js';
-
+var openProgram= 'none';
 $( window ).load(function() {
   $('#main').html(summer(1000));
 
 });
+
+function loadProgram(program){
+  $('#main').html(app[program]);
+}
