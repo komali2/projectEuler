9efdd983f49d5391179370f7416b13e07d454307
komali2 <rogersjcaleb@gmail.com>
2016-05-18 16:45:07 -0700

    core: added prime sieve

--- projects/largestPrimeFactor.js
+++ projects/largestPrimeFactor.js
@@ -3,9 +3,37 @@
 // What is the largest prime factor of the number 600851475143 ?
 
 function primeFinder(target){
-
+  //check each number for primeness up to the target
 }
 
+//generate an array of primes using Sieve of Eratosthenes
+function sieve(n){
+  var array = [];
+  var upper = Math.sqrt(n);
+  var arrayOut = [];
+
+  //make array from 2 to (n - 1)
+  for(var i = 0; i < n; i++){
+    array.push(true);
+  }
+
+  //remove multiples of primes starting from 2, 3, 5...
+  for(var i = 2; i <= upper; i++){
+    if(array[i]){
+      for(var j = i * i; j < n; j += i){
+        array[j] = false;
+      }
+    }
+  }
+
+  //all array[i] set to true are primes
+  for(var i = 2; i < n; i++){
+    if(array[i]){
+      arrayOut.push(i);
+    }
+  }
+  return output;
+}
 
 
 
